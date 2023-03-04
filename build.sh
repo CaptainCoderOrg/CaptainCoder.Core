1312e92bb882e4b3cb7a6265247ecc56bc34a4c9
Joseph Collard <jcollard@users.noreply.github.com>
2023-03-03 19:16:22 -0700

    chore: Use release build dll.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-dotnet build
-cp CaptainCoder/Core/bin/Debug/netstandard2.1/CaptainCoder.dll ../RPG\ Crafting\ System/Assets/Libs/CaptainCoder.dll
+dotnet build --configuration Release
+cp CaptainCoder/Core/bin/Release/netstandard2.1/CaptainCoder.dll ../RPG\ Crafting\ System/Assets/Libs/CaptainCoder.Core.dll

04b1e37395019946a15f067e2f2d9004911f489f
Joseph Collard <jcollard@users.noreply.github.com>
2023-03-06 11:32:01 -0700

    chore: Change output name in build.sh.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 dotnet build --configuration Release
-cp CaptainCoder/Core/bin/Release/netstandard2.1/CaptainCoder.dll ../RPG\ Crafting\ System/Assets/Libs/CaptainCoder.Core.dll
+cp CaptainCoder/Core/bin/Release/netstandard2.1/CaptainCoder.dll ../RPG\ Crafting\ System/Assets/Plugins/CaptainCoder.dll

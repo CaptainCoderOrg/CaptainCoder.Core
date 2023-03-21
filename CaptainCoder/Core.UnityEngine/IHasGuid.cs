e77af7c91684f6ddb695acb6b02ecc79568ddca7
Joseph Collard <jcollard@users.noreply.github.com>
2023-03-21 06:05:12 -0700

    feat: Add IHasGuid.RegenerateGuid.

--- CaptainCoder/Core.UnityEngine/IHasGuid.cs
+++ CaptainCoder/Core.UnityEngine/IHasGuid.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using UnityEngine;
 using UE = UnityEngine;
-
 namespace CaptainCoder.Core.UnityEngine;
+
 /// <summary>
 /// The <see cref="IHasGuid"/> interface provides a single string property
 /// representing a unique id. By default, this interface provides an
@@ -16,20 +16,22 @@ public interface IHasGuid : ISerializationCallbackReceiver
     /// Represents a unique string identifier
     /// </summary>
     public string Guid { get; protected set; }
+
+    /// <summary>
+    /// Regenerates the Guid for this object
+    /// </summary>
+    public void RegenerateGuid()
+    {
+        if (this is UE.Object ueo) { Debug.Log($"Generating GUID for {this}", ueo); }
+        else { Debug.Log($"Generating GUID for {this}"); }
+        Guid = System.Guid.NewGuid().ToString();
+    }
+
     void ISerializationCallbackReceiver.OnBeforeSerialize()
     {
-        // throw new System.NotImplementedException();
         if (string.IsNullOrEmpty(Guid?.Trim()))
         {
-            if (this is UE.Object ueo)
-            {
-                Debug.Log($"Generating GUID for {this}", ueo);
-            }
-            else
-            {
-                Debug.Log($"Generating GUID for {this}");
-            }
-            Guid = System.Guid.NewGuid().ToString();
+            RegenerateGuid();
         }
     }
 

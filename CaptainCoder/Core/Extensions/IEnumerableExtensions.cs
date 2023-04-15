f9a3ef6e46db203349a311663168f00e768b84d6
Joseph Collard <jcollard@users.noreply.github.com>
2023-04-15 09:08:38 -0700

    feat: Implement IEnumerableExtensions.Shuffle

---
+++ CaptainCoder/Core/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+namespace CaptainCoder.Core;
+
+/// <summary>
+/// A set of extension methods for <see cref="IEnumerable{T}"/>
+/// </summary>
+public static class IEnumerableExtensions
+{
+
+    /// <summary>
+    /// Given an <see cref="IEnumerable{T}"/>, randomizes the order of elements.
+    /// </summary>
+    public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> toShuffle) => Shuffle<T>(toShuffle, IRandom.Shared);
+
+    /// <summary>
+    /// Given an <see cref="IEnumerable{T}"/>, randomizes the order of elements using the specified <paramref name="randomSource"/>
+    /// </summary>
+    public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> toShuffle, IRandom randomSource) => toShuffle.OrderBy(_ => randomSource.NextDouble());
+}

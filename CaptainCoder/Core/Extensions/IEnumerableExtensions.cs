c19758f0f469715f54f6c28fad67c6732eca35b4
Joseph Collard <jcollard@users.noreply.github.com>
2023-05-16 16:49:10 -0700

    feat: Implement ForEach on IEnumerable.

--- CaptainCoder/Core/Extensions/IEnumerableExtensions.cs
+++ CaptainCoder/Core/Extensions/IEnumerableExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 namespace CaptainCoder.Core;
@@ -17,4 +18,15 @@ public static class IEnumerableExtensions
     /// Given an <see cref="IEnumerable{T}"/>, randomizes the order of elements using the specified <paramref name="randomSource"/>
     /// </summary>
     public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> toShuffle, IRandom randomSource) => toShuffle.OrderBy(_ => randomSource.NextDouble());
+
+    /// <summary>
+    /// Given an <see cref="IEnumerable{T}"/>, peforms the specified <paramref name="action"/> to each element.
+    /// </summary>
+    public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+    {
+        foreach (T element in enumerable)
+        {
+            action.Invoke(element);
+        }
+    }
 }
